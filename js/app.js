bb721def8fe1d7e25488dc7f2992cb4da45f4c92
tobias <tobiaszkedzierski@gmail.com>
2018-03-24 14:36:18 +0100

    drop down menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-  // tutaj będziemy umieszczać kod ze wszystkich zadań
-  console.log('działa');
+document.addEventListener('DOMContentLoaded', function() {  // console.log('działa');
+
+  var forDropdown = document.querySelector('.for-dropdown');
+  var dropdown = document.querySelector('.dropdown');
+
+  forDropdown.addEventListener('mouseover', function() {
+      dropdown.style.display = 'block';
+      // set width of dropdown menu due to parrent button
+      dropdown.style.width = String(forDropdown.offsetWidth) + "px";
+  });
+
+  forDropdown.addEventListener('mouseout', function() {
+      dropdown.style.display = 'none';
+  });
+
 });
