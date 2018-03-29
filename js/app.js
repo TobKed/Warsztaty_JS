11c5eb425c3387367acf3d098a28d1678c4dc7a6
tobias <tobiaszkedzierski@gmail.com>
2018-03-29 18:24:59 +0200

    fix task list

--- js/app.js
+++ js/app.js
@@ -91,20 +91,17 @@ document.addEventListener('DOMContentLoaded', function() {  // console.log('dzia
 
     // TASKS LIST
     var addToListButton = document.querySelector('.input-group-btn');
-    var tasksList = document.querySelector('.list-group');
-    var newTaskInput = document.querySelector('.form-control');
-    var newTask = document.createElement('li');
 
     addToListButton.addEventListener('click', addTask);
 
     function addTask() {
-
-        console.log(newTask);
-        tasksList.appendChild(newTask);
+        var tasksList = document.querySelector('.list-group');
+        var newTaskInput = document.querySelector('.form-control');
+        var newTask = document.createElement('li');
         newTask.innerHTML = newTaskInput.value;
         newTaskInput.value = '';
-        console.log(tasksList);
-        tasksList = document.querySelector('.list-group');
+        tasksList.appendChild(newTask);
+        // tasksList = document.querySelector('.list-group');
 
     };
 
